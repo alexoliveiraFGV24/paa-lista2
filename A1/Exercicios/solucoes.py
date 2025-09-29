19bae0e9ca1a0ec715fb5c31cda35f07d84b17a1
ALEX JÚNIO MAIA DE OLIVEIRA <B435917@fgv.edu.br>
2025-09-29 10:52:57 -0300

    Adicionando todas as questões

---
+++ A1/Exercicios/solucoes.py
@@ -0,0 +1,26 @@
+""" A1 2022 questão 2 """
+def q2_2022(A, x):
+    pass
+
+
+
+
+
+
+""" A1 2022 questão 3; A1 2024 questão 4 """
+
+""" A1 2022 questão 5 """
+
+""" A1 2022 questão 6 """
+
+""" A1 2023 questão 2 """
+
+""" A1 2023 questão 3 """
+
+""" A1 2023 questão 4; Simulado A1 2025 questão 2 """
+
+""" A1 2023 questão 5; Simulado A1 2025 questão 4 """
+
+""" A1 2024 questão 3 """
+
+""" Simulado A1 2025 questão 3 """
